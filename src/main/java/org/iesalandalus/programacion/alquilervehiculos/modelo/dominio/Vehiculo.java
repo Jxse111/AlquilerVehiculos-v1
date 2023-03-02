6e53f44af81166f33274aa33805d9390c6d541db
Jxse111 <josemartinezestrada111@gmail.com>
2023-03-02 14:21:39 +0100

    He creado el equals con instanceof

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Vehiculo.java
@@ -1,15 +1,17 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
 
-public class Vehiculo {
+import java.util.Objects;
+
+public abstract class Vehiculo {
 
 	private static final String ER_MARCA = "(Seat|Land Rover|KIA|Rolls-Royce|SsangYong)";
 	private static final String ER_MATRICULA = "[0-9]{4}[BCDFGHJKLMNÑPQRSTVWXYZ]{3}";
-	protected String marca;
-	protected String modelo;
-	protected String matricula;
+	private String marca;
+	private String modelo;
+	private String matricula;
 
 	public static Turismo getTurismoConMatricula(String matricula) {
-		return new Turismo ("Seat","León", 90, matricula);
+		return new Turismo("Seat", "León", 90, matricula);
 	}
 
 	public String getMarca() {
@@ -20,7 +22,7 @@ public class Vehiculo {
 		if (marca == null) {
 			throw new NullPointerException("ERROR: la marca no puede ser nula.");
 		}
-	
+
 	}
 
 	public String getModelo() {
@@ -47,16 +49,22 @@ public class Vehiculo {
 		throw new IllegalArgumentException("Matrícula no tiene un formato válido");
 	}
 
-	public boolean equals(Object o) {
-		if (Object instanceof o);
-	
-		Turismo turismo = (Turismo) o;
-	
-		return matricula.equals(turismo.matricula);
+	public abstract double getFactorPrecio();
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(marca, matricula, modelo);
 	}
 
-	public int hashCode() {
-		return matricula.hashCode();
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Vehiculo))
+			return false;
+		Vehiculo other = (Vehiculo) obj;
+		return Objects.equals(marca, other.marca) && Objects.equals(matricula, other.matricula)
+				&& Objects.equals(modelo, other.modelo);
 	}
 
 	public Vehiculo() {
