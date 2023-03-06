f7cd9a9bca1cbecf758ec1b5b4b44d89cf61c78b
jmfdiazAL <josemanuel.fernandez@iesalandalus.org>
2023-03-06 13:40:24 +0100

    Habia extendido la clase autobus de la clase de vehiculo sin querer y he tenido que volver a copiar mi codigo anterior de vehiculo

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Vehiculo.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Vehiculo.java
@@ -1,14 +1,51 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
 
+import java.util.Objects;
 
+public abstract class Vehiculo {
 
-public abstract class Vehiculo extends Autobus {
+	private static final String ER_MARCA = "(Seat|Land Rover|KIA|Rolls-Royce|SsangYong)";
+	private static final String ER_MATRICULA = "[0-9]{4}[BCDFGHJKLMNÑPQRSTVWXYZ]{3}";
+	protected String marca;
+	protected String modelo;
+	protected String matricula;
 
-	protected Vehiculo(Vehiculo Vehiculo) {
-		super();
+	public static Turismo getTurismoConMatricula(String matricula) {
+		return new Turismo("Seat", "León", 90, matricula);
+
 	}
-	public Vehiculo(String marca2, String modelo2, String matricula2) {
-		super();
+
+	public String getMarca() {}
+
+	public void setMarca(String marca) throws IllegalArgumentException {
+		if (marca == null) {
+			throw new NullPointerException("ERROR: la marca no puede ser nula.");
+		}
+	}
+	
+
+	public String getModelo() {
+
+	public void setMatricula(String matricula) throws IllegalArgumentException {
+		throw new IllegalArgumentException("Matrícula no tiene un formato válido");
+	}
+	
 	}
 
-}
+	public abstract double getFactorPrecio();
+
+	@Override
+	public int hashCode() {
+		return matricula.hashCode();
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Vehiculo))
+			return false;
+		Vehiculo other = (Vehiculo) obj;
+		return Objects.equals(marca, other.marca) && Objects.equals(matricula, other.matricula)
+				&& Objects.equals(modelo, other.modelo);
+	}
+	}
