2643af93609bc051b003a612b7492e26860c4574
José Martínez <116352794+Jxse111@users.noreply.github.com>
2023-03-14 10:38:45 +0100

    Add files via upload

---
+++ AlquilerVehiculos-v1/src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/dominio/Vehiculo.java
@@ -0,0 +1,69 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.dominio;
+
+import java.util.Objects;
+
+public abstract class Vehiculo extends Autobus {
+	private static final String ER_MARCA = "(Seat|Land Rover|KIA|Rolls-Royce|SsangYong)";
+	private static final String ER_MATRICULA = "[0-9]{4}[BCDFGHJKLMNÑPQRSTVWXYZ]{3}";
+	private String marca;
+	private String modelo;
+	private String matricula;
+
+	protected Vehiculo(String marca, String modelo, int plazas, String matricula) {
+		super(marca, modelo, plazas, matricula);
+	}
+
+	protected Vehiculo Vehiculo(Vehiculo vehiculo) {
+		 return vehiculo;
+	}
+
+	public Vehiculo copiar(Vehiculo vehiculo) {
+		return vehiculo;
+	}
+
+	protected static Turismo getTurismoConMatricula(String matricula) {
+		return vehiculo;
+	}
+
+	public abstract int getFactorPrecio();
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca() {
+		if (marca == null) {
+			throw new NullPointerException("ERROR: la marca no puede ser nula.");
+		}
+	}
+
+	public String getModelo() {
+		return modelo;
+
+	}
+
+	public void setModelo(String matricula) throws IllegalArgumentException {
+		throw new IllegalArgumentException("Matrícula no tiene un formato válido");
+
+	}
+
+	public void setMatricula(String matricula) throws IllegalArgumentException {
+		throw new IllegalArgumentException("Matrícula no tiene un formato válido");
+	}
+
+	@Override
+	public int hashCode() {
+		return matricula.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Vehiculo))
+			return false;
+		Vehiculo other = (Vehiculo) obj;
+		return Objects.equals(marca, other.marca) && Objects.equals(matricula, other.matricula)
+				&& Objects.equals(modelo, other.modelo);
+	}
+}
---
+++ AlquilerVehiculos-v1/src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/negocio/Turismos.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.alquilervehiculos.modelo.negocio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
+
+public class Turismos extends Vehiculos {
+
+	List<Turismo> coleccionTurismo;
+
+	public Turismos() {
+
+		super();
+		coleccionTurismo = new ArrayList<>();
+
+	}
+
+}
